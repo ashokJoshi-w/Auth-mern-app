d40d6088f44d95517d275c2256c97d18a4093b3a
ashok <ashokjoshi2451@gmail.com>
2026-05-08 23:16:39 +0530

    Mongo database

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
  
+app.get('/ping', (req, res)=>{
+    res.send('PONG');
+})
 app.listen(PORT, ()=> {
     console.log('Server is running on ${PORT}')
 })
